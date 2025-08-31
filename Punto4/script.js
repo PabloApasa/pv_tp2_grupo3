1511bba466842be5078f14139c8f2f49a0bd9447
AldiRQ <negronitica@gmail.com>
2025-08-30 22:04:37 -0300

    Eliminar comentarios de script.js

--- Punto4/script.js
+++ Punto4/script.js
@@ -1,19 +1,19 @@
      const boton = document.getElementById("mostrarBtn");
-const libretaInput = document.getElementById("libreta");
+     const libretaInput = document.getElementById("libreta");
 
-// Validar que solo se ingresen números en libreta
+
 libretaInput.addEventListener("input", function() {
   this.value = this.value.replace(/[^0-9]/g, '');
 });
 
-// Agregar evento al botón
-boton.addEventListener("click", function() {
-  // Obtenemos valores de los inputs
-  const nombre = document.getElementById("nombre").value;
-  const apellido = document.getElementById("apellido").value;
-  const libreta = libretaInput.value;
 
-  // Mostramos en un alert
+boton.addEventListener("click", function() {
+  
+     const nombre = document.getElementById("nombre").value;
+     const apellido = document.getElementById("apellido").value;
+     const libreta = libretaInput.value;
+
+  
   alert("Los datos ingresados son:\n" +
         "Nombre: " + nombre + "\n" +
         "Apellido: " + apellido + "\n" +
