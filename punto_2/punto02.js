8b4c28b60af7cbd3e02720b4f066667849829400
PabloApasa <143457610+PabloApasa@users.noreply.github.com>
2025-09-01 11:39:00 -0300

    actualizacion punto 2- apasa pablo

--- punto_2/punto02.js
+++ punto_2/punto02.js
@@ -3,13 +3,11 @@ function calcularPromedio() {
     const num2 = parseInt(document.getElementById("num2").value);
     const num3 = parseInt(document.getElementById("num3").value);
 
-    // Si alguno de los valores no es un número, se muestra una alerta y la función termina.
     if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
         alert("Por favor, ingrese números válidos");
         return;
     }
 
-    // Este código solo se ejecutará si la condición anterior es falsa.
     const promedio = (num1 + num2 + num3) / 3;
 
     const resultadoElement = document.getElementById("resultado");
